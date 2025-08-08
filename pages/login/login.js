58afaa460d622fc2c3110953a3b0327097707f44
Paula Mansilla <pimansilla@scl.edu.gt>
2025-08-07 23:57:21 -0600

    avanzado

--- pages/login/login.js
+++ pages/login/login.js
@@ -1,3 +1,5 @@
+import { banner } from "../../componentes/banner/banner.js";
+
 export function login(){
     let div = document.createElement('div');
     div.className = "login";
@@ -18,10 +20,15 @@ export function login(){
     div.appendChild(span3);
 
     let boton1 = document.createElement('a');
-    boton1.href = "../login2.html";
+    boton1.href = "../bannerLogin/bannerLogin.html";
     boton1.className = "boton1"
     boton1.textContent = "LOG IN"
     div.appendChild(boton1);
+
+    boton1.addEventListener('click', ()=>{
+        div.classList.add = ("ocultar");
+        document.body.appendChild(banner());
+    });
 
     let span4 = document.createElement('span');
     span4.className = "span4"
